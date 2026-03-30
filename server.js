57fc0f5f1962ca1074fd17a26019be4f7b10d994
amollica-analytics <aj4hockey@gmail.com>
2026-03-30 16:13:14 -0400

    Fixed setup.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const { db, User, Task } = require('./setup');
+const { db, User, Task } = require('./database/setup');
 require('dotenv').config();
 
 const app = express();
